434da5cb5d365a55ba1b5d2b5285a2f3f66b0227
Tiger Fok <tiger.indzz@gmail.com>
2017-03-17 16:07:42 +0800

    Updated view

--- src/resources/views/laravel-settings-ui.blade.php
+++ src/resources/views/laravel-settings-ui.blade.php
@@ -1,19 +1,17 @@
-@section('content')
-    @if (session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
-        </div>
-    @endif
+@if (session('status'))
+	<div class="alert alert-success">
+		{{ session('status') }}
+	</div>
+@endif
 
-    <div class="main-container-padding">
-        {!! form_start($form) !!}
+<div class="container">
+	{!! form_start($form) !!}
 
-        {!! form_rest($form) !!}
+	{!! form_rest($form) !!}
 
-        <div class="form-footer">
-            <button type="submit" class="btn-success btn">{{ trans('laravel-settings-ui.button.save') }}</button>
-        </div>
+	<div class="form-footer">
+		<button type="submit" class="btn-success btn">{{ trans('laravel-settings-ui.button.save') }}</button>
+	</div>
 
-        {!! form_end($form) !!}
-    </div>
-@endsection
+	{!! form_end($form) !!}
+</div>
