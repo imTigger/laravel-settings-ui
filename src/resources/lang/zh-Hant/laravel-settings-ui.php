3d310772d68fb649d9d4962583b0eb049476d2e2
Tiger Fok <tiger.indzz@gmail.com>
2017-03-17 17:48:12 +0800

    Added translations

---
+++ src/resources/lang/zh-Hant/laravel-settings-ui.php
@@ -0,0 +1,10 @@
+<?php
+
+return array (
+  'message' => [
+    'saved' => '設定儲存成功'
+  ],
+  'button' => [
+	'save' => '儲存'
+  ]
+);
---
+++ src/resources/lang/zh/laravel-settings-ui.php
@@ -0,0 +1,10 @@
+<?php
+
+return array (
+  'message' => [
+    'saved' => '设定储存成功'
+  ],
+  'button' => [
+	'save' => '储存'
+  ]
+);
